060a208a088bd4ae78948ce6a03658f02cdf6f73
吉田紀幸 <n.yoshida@trifort.jp>
2015-05-15 19:27:11 +0900

    テストプロジェクト

---
+++ uGUITest/Assets/MyButton.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MyButton : MonoBehaviour {
+
+	private int clickCount = 0;
+	private Text text = null;
+
+
+	void Start(){
+
+		text = gameObject.transform.Find ("Text").GetComponent<Text>();
+	}
+
+	/// ボタンをクリックした時の処理
+	public void OnClick() {
+
+		text.text = "Click " + clickCount;
+		clickCount++;
+	}
+}
---
+++ uGUITest/Assets/test.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GameObject.Find("Canvas/Text").GetComponent<Text>().text = "テキスト変更";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
